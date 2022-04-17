063110380debb90564fabb3434298386eecc2d5c
wangj98 <1487812189@qq.com>
2022-04-17 14:36:45 +0800

    update

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
+        String content="快点起床";
+        String filePath =Request.sendRequest(content);
+        Player player = new Player();
+        player.Play(filePath);
+
         Client client = new Client();
         client.Test();
     }
--- src/Client.java
+++ src/Client.java
@@ -3,12 +3,7 @@ package src;
 public class Client {
     public void Test() {
         System.out.println("This is Client");
-        String content="快点起床学习啦啦啦啦啦";
-        String filePath =Request.sendRequest(content);
-        Player player = new Player();
-        player.Play(filePath);
 
-        
 
 
     }
