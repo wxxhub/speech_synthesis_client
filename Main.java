585b3b78ad714ef8f6366dee8ff32934d5ac720c
wangj98 <1487812189@qq.com>
2022-04-17 21:56:06 +0800

    update-03

--- Main.java
+++ Main.java
@@ -3,10 +3,30 @@ import src.Client;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String url = "http://127.0.0.1:8888/speech_synthesis";
+        String content = "你好";
+        String savePath = "C:\\Users\\ADMIN\\Desktop\\语音\\1.wav";
+        if (args != null) {
+            for (String arg : args) {
+                if (arg.matches("-save=.*")) {
+                    savePath = arg.replace("-save=", "");
+                    System.out.println("savePath=" + savePath);
+                }
+                if (arg.matches("-h=.*")) {
+                    url = arg.replace("-h=", "");
+                    System.out.println("url=" + url);
+                }
+                if (arg.matches("-c=.*")) {
+                    content = arg.replace("-c=", "");
+                    System.out.println("content=" + content);
+                }
+                if (arg.matches("-d=.*")) {
+                }
 
-        String savePath ="C:\\Users\\ADMIN\\Desktop\\语音\\1.wav";
+            }
+        }
         Client client = new Client();
-        client.generate("你好",savePath);
+        client.generate(content, savePath, url);
         client.test(savePath);
 
     }
